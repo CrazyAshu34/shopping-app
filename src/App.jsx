a4fab3e08a532170a55a450ce93dcafefbc52fa3
ashutosh-dgksocial <ashutoshdgksocial@gmail.com>
2025-04-13 18:32:46 +0530

    learned useMemo hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,36 @@
-import "./App.css";
+import React, { useState, useCallback } from "react";
 import MyComponent from "./components/MyComponent";
+
 const App = () => {
+  const [count, setCount] = useState(0);
+
+  console.log("👱‍♂️ Parent rendered");
+
+  // const handleClick = useCallback(() => {
+  //   console.log("🧠 useCallback function ran");
+  // }, []);
+
+  const handleClick = () => {
+    console.log("🧠 useCallback with usecallback function ran");
+  };
+
   return (
-    <>
-      <div className="border p-4 ">
-        <p className="pb-5 text-center">APP - PAGE</p>
-        <hr />
-        <br />
-        <MyComponent />
+    <div className="p-5 min-h-screen">
+      <h1 className="text-3xl font-bold mb-4 text-center">Welcome to My App</h1>
+      <p className="text-center mb-6">
+        ----------------------------------------
+      </p>
+      <div className="border-2 border-white h-[70vh] w-full rounded-lg">
+        {/* Child component will be placed here */}
+        <p> _Parent btn</p>
+        {/* if any re-render caused by parent button the child component not rerendred */}
+        <button onClick={() => setCount(count + 1)}>
+          count click: {count}
+        </button>
+        <MyComponent handleClick={handleClick} />
       </div>
-    </>
+    </div>
   );
 };
+
 export default App;
--- src/components/MyComponent.jsx
+++ src/components/MyComponent.jsx
@@ -1,50 +1,14 @@
-import { useEffect, useMemo, useRef, useState } from "react";
-
-// 🔥 Heavy calculation (slow)
-const slowFunction = (num) => {
-  // console.log("Calculating...");
-  let result = 0;
-  for (let i = 0; i < 100000000; i++) {
-    result += i;
-  }
-  return result + num; // 100000000 + 1 = 100000001 demo
-};
-
-const MyComponent = () => {
-  const [count, setCount] = useState(0);
-  const [load, setLoad] = useState(false);
-  const myRef = useRef(null);
-  console.log("1st", typeof myRef);
-  // debounce
-  const handleCount = () => {
-    if (myRef.current) clearTimeout(myRef.current);
-
-    myRef.current = setTimeout(() => {
-      setCount((prev) => prev + 1);
-    }, 500);
-  };
-
-  useEffect(() => {
-    if (load) return;
-    setLoad(true);
-    const id = setTimeout(() => setLoad(false), 500);
-    return () => clearTimeout(id);
-  }, [count]);
-
-  const expensiveValue = useMemo(() => slowFunction(count), [count]); // use memo
-
+import React, { useMemo } from "react";
+const MyComponent = ({ handleClick }) => {
+  console.log("👶 Child rendered");
   return (
-    <div style={{ padding: "20px" }}>
-      <h2>useMemo Example</h2>
-      {!load ? <p>Count: {count}</p> : <p>Loading...</p>}
-      {/* <p>Count: {count}</p> */}
-      <p>Expensive Calculation Result: {expensiveValue}</p>
-      <button disabled={load} onClick={handleCount}>
-        {load ? "Processing..." : "Increment ++"}
-      </button>
-      <h2>useMemo Example</h2>
+    <div>
+      <br />
+      <p>_Child btn</p>
+      <button onClick={handleClick}>i am clicking in it</button>
     </div>
   );
 };
 
-export default MyComponent;
+// export default MyComponent;
+export default React.memo(MyComponent);
